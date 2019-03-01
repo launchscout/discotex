08f4d29018f21d05a5b39ab078df98a7be473cc1
Tim Mecklem <timothy@mecklem.com>
2019-03-01 10:35:25 -0500

    oops

--- apps/discotex_bot/config/config.exs
+++ apps/discotex_bot/config/config.exs
@@ -21,9 +21,6 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-config :nostrum,
-  token: "NTUwMzAzODMwNjg0ODYwNDI4.D1geAA.8AlLiye3oDV9F8AAwFDFm7XAX38"
-
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
