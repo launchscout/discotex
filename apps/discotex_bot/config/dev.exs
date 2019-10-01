d0278b25cd358bc9f6a6213292fb2ee4f156670a
Tim Mecklem <timothy@mecklem.com>
2019-10-01 12:41:59 -0400

    Allow discotex to run in development mode

--- apps/discotex_bot/config/dev.exs
+++ apps/discotex_bot/config/dev.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :discotex_bot, DiscotexBot.SimpleResponder, enabled: false
+config :discotex_bot, DiscotexBot.SimpleResponder, enabled: true
 
 config :nostrum,
-  token: ""
+  token: System.get_env("DISCORD_DEV_TOKEN")
