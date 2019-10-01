9d840d3a8781fb1ec09a9e42a3678bd8edc814c3
Tim Mecklem <timothy@mecklem.com>
2019-10-01 12:42:12 -0400

    Add failing bot test for polls

---
+++ apps/discotex_bot/test/poll_test.exs
@@ -0,0 +1,30 @@
+defmodule PollTest do
+  use ExUnit.Case
+
+  alias DiscotexBot.Dispatch
+  alias Nostrum.Struct.{Message, User}
+
+  doctest Dispatch
+
+  @user_id "Rit Tomney1233533"
+  test "Discotex keeps a running count of reactions" do
+    message = %Message{
+      content: "hi",
+      author: %User{id: @user_id},
+      channel_id: 5,
+      id: 6,
+      reactions: [
+        %Nostrum.Struct.Message.Reaction{
+          count: 2,
+          emoji: %Nostrum.Struct.Emoji{id: nil, name: "💯"}
+        },
+        %Nostrum.Struct.Message.Reaction{
+          count: 1,
+          emoji: %Nostrum.Struct.Emoji{id: 243, name: "oof"}
+        }
+      ]
+    }
+
+    assert DiscotexBot.get_poll_results(message.channel_id, message.id) == {:ok, %{content: "💯 is the winning reaction"}}
+  end
+end
