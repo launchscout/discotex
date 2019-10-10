1a75f363ea71178f4c5b25b9eac35e95f5e4bfea
Tim Mecklem <timothy@mecklem.com>
2019-10-10 18:09:29 -0400

    Add enlighten me

--- apps/discotex_bot/lib/discotex_bot/responders/random_responder.ex
+++ apps/discotex_bot/lib/discotex_bot/responders/random_responder.ex
@@ -8,7 +8,8 @@ defmodule DiscotexBot.Responders.RandomResponder do
 
   @message_types [
     {~r/\badventure me\b/i, :adventure_me},
-    {~r/\bcatbug me\b/i, :catbug_me}
+    {~r/\bcatbug me\b/i, :catbug_me},
+    {~r/\benlighten me\b/i, :enlighten_me}
   ]
 
   def responds_to?(message) do
@@ -72,6 +73,26 @@ defmodule DiscotexBot.Responders.RandomResponder do
   defp do_reply({:catbug_me, message}) do
     {:message_create, Enum.random(@catbugs), message.channel_id}
   end
+
+  @buddha [
+    "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+    "Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.",
+    "Three things cannot be long hidden: the sun, the moon, and the truth.",
+    "There are only two mistakes one can make along the road to truth; not going all the way, and not starting.",
+    "The mind is everything. What you think you become.",
+    "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
+    "No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.",
+    "It is better to travel well than to arrive.",
+    "You, yourself, as much as anybody in the entire universe, deserve your love and affection.",
+    "Just as treasures are uncovered from the earth, so virtue appears from good deeds, and wisdom appears from a pure and peaceful mind. To walk safely through the maze of human life, one needs the light of wisdom and the guidance of virtue.",
+    "You will not be punished for your anger, you will be punished by your anger.",
+    "We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves.",
+    "Peace comes from within. Do not seek it without."
+  ]
+  defp do_reply({:enlighten_me, message}) do
+    {:message_create, Enum.random(@buddha), message.channel_id}
+  end
+
 
   defp map_message(message = %Message{content: content}) do
     {_, type} =
